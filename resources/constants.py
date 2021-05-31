eb17fdfb3d94ed9640df46e273a749798a839ed5
Justin <jumullins@comcast.net>
2021-05-31 09:15:48 +0100

    updates

--- resources/constants.py
+++ resources/constants.py
@@ -13,7 +13,7 @@ LOGOUT = os.path.abspath("resources/images/log-out-02.png")
 COLOR = os.path.abspath("resources/images/color.jpg")
 DELETE = os.path.abspath("resources/images/delete.png")
 MOVIE = os.path.abspath("resources/images/movie.png")
-FOOD = os.path.abspath("resources/images/food.png")
+FOOD = os.path.abspath("resources/images/food2.png")
 
 # JSON
 BACKEND = os.path.abspath("resources/backend/")
